a964a158adf3ebb91aa1b331c575f6eb6089a35b
Daniel Feliciano <felicdan@berkeleyprep.org>
2018-08-29 16:52:19 -0400

    Home work 0 fix

--- Homework0.java
+++ Homework0.java
@@ -10,10 +10,7 @@ public class Homework0 {
 	 * Ensure that you match case and punctuation exactly
 	 */
 	public static void main(String[] args) {
-		// The S in System needs to be capitalized; it is a special
-		//  case of a time when we access members of a class rather
-		//  than an object
-   system.out.println("Hello, World!");
+   System.out.println("Hello, World!");
 	}
 
 }
